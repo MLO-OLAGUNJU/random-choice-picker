dac60099067214984ff3592267dbd1bd9ba867f2
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2024-01-05 15:38:50 +0100

    I modified app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const tagsElement = document.querySelector("#tags");
+const textAreaElement = document.querySelector("#textarea");
+const theTagElement = document.querySelector("#tag");
+
+// whenever i go to the page, the cursor will be always in the textarea
+textAreaElement.focus();
+
+textAreaElement.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+const createTags = (input) => {
+  const tags = input.split(",");
+  console.log(tags);
+};
